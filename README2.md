f3835a5ce9b1b11919f2418c80c3201ab9744726
ilyass elassal <ilyass9472@gmail.com>
2025-03-11 12:12:14 +0000

    autentification avec creation d'une wallet et du user au meme temps

---
+++ README2.md
@@ -0,0 +1,13 @@
+first you shold to install this project and set it in your envirement
+
+next step install composer :    composer install
+
+next step :   composer dump-autoload
+
+
+next step instal node.js : npm install
+
+
+next step run node.js : npm run dev
+
+
---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+class AuthController extends Controller
+{
+    public function register(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|string|min:6|confirmed'
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'errors' => $validator->errors()
+        ], 422);
+    }
+    try{
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+        'data' => [
+            'user' => $user,
+            'token' => $token
+        ]
+    ], 201);
+}catch(\Exception $e){
+    // dd($e->getMessage());
+    return response()->json([
+        'message'=> 'there is something wrong in creation du user pls resoudre votre probléeme'. $e->getMessage()
+    ],500);
+    
+}
+}
+
+public function login(Request $request)
+{
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+    return response()->json(['token' => $token], 200);
+}
+
+public function logout(Request $request)
+{
+    $request->user()->tokens()->delete();
+    return response()->json(['message' => 'Logged out'], 200);
+}
+
+}
